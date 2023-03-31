a8c475df536dffd18b0e42f2af0f2b07dd89b69d
JordanKirch <93733282+JordanKirch@users.noreply.github.com>
2023-03-30 20:27:22 -0700

    Data can be inputted into SQL and can be plotted.

---
+++ Stock_Market_Analysis.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf
+import sqlite3
+
+# Connect to a local SQLite database to store the stock market data
+conn = sqlite3.connect('stock_data.db')
+
+# Use yfinance to get historical stock market data for a particular stock
+msft = yf.Ticker('MSFT')
+msft_data = msft.history(period='max')
+
+# Write the data to a table in the database
+msft_data.to_sql('msft', conn, if_exists='replace')
+
+# Use SQL to wrangle the data (example: filter out missing values)
+df = pd.read_sql_query('SELECT * FROM msft WHERE Close > 0', conn)
+
+# Perform data analysis using Python libraries like pandas, numpy, and matplotlib
+mean_price = np.mean(df['Close'])
+plt.plot(df['Date'], df['Close'])
+plt.title('Microsoft Stock Price Over Time')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
+# Use Python to build a predictive model
+# TODO learn scikit-learn
+from sklearn.linear_model import LinearRegression
+
+X = np.array(df.index).reshape(-1, 1)
+y = np.array(df['Close'])
+
+model = LinearRegression()
+model.fit(X, y)
+
+# Use the model to make predictions
+next_day = df.index[-1] + pd.DateOffset(1)
+prediction = model.predict([[next_day]])[0]
+print(f'The predicted stock price for {next_day.date()} is {prediction:.2f}')
